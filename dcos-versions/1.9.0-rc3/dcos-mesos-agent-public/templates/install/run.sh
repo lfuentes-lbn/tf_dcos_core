f00b280c37697e451df96eb80b68bb574d06c3c6
peshane <peshane@peshane.net>
2018-03-09 10:40:06 +0100

    add exit 0 if DC/OS already there to avoid error

--- dcos-versions/1.9.0-rc3/dcos-mesos-agent-public/templates/install/run.sh
+++ dcos-versions/1.9.0-rc3/dcos-mesos-agent-public/templates/install/run.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
+# exit 0 if an already installed dcos is there
+systemctl is-active --quiet dcos-* && echo "DC/OS seems already installed on $HOSTNAME, exit O" && exit 0
+
 # Install Agent Node
 mkdir /tmp/dcos && cd /tmp/dcos
 /usr/bin/curl -O ${bootstrap_private_ip}:${dcos_bootstrap_port}/dcos_install.sh
